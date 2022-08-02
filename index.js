11c23d02a9283a2b2dd8437386cd988bc69140d3
Koray Uymaz <korayuymaz@gmail.com>
2022-08-02 14:04:17 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let myLibrary = [];
+
+/* 
+author: author,
+title: title,
+number_of_pages: pages,
+read: false,
+*/
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  
+}
+
+for(let book in myLibrary){
+  console.log(book)
+}
