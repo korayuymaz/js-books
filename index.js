4b91742049caa730aa15a0f114d142f4d3878bca
Koray Uymaz <korayuymaz@gmail.com>
2022-08-03 15:25:54 +0300

    docs added js structure created

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
+let form = document.querySelector("form");
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let pages = document.querySelector("#pages");
+let bookcase = document.querySelector("#bookcase");
+
+
 let myLibrary = [];
 
-/* 
-author: author,
-title: title,
-number_of_pages: pages,
-read: false,
-book-case-url: https://images-na.ssl-images-amazon.com/images/I/81qICU76srL.__AC_SX300_SY300_QL70_ML2_.jpg,
-*/
+function Book(){
 
-function Book() {
-  // the constructor...
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(){
   
-}
-
-for(let book in myLibrary){
-  console.log(book)
 }
