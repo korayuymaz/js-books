462a05288408511333ccf502da0b18e1b42d7053
Koray Uymaz <korayuymaz@gmail.com>
2022-08-03 10:16:47 +0300

    Styling is complete

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ author: author,
 title: title,
 number_of_pages: pages,
 read: false,
+book-case-url: https://images-na.ssl-images-amazon.com/images/I/81qICU76srL.__AC_SX300_SY300_QL70_ML2_.jpg,
 */
 
 function Book() {
