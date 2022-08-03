d6a614463a68e50775fa54ab7f619c3bb206146a
Koray Uymaz <korayuymaz@gmail.com>
2022-08-03 19:40:49 +0300

    Final commit before tests

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ let author = document.querySelector("#author");
 let pages = document.querySelector("#pages");
 let bookcase = document.querySelector("#bookcase");
 let submitButton = document.querySelector("#submit")
-
+let books = document.querySelector(".books")
 
 let myLibrary = [];
 
-function Book(title, author, pages, bookcase, read){
+function Book(id, title, author, pages, bookcase, read){
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -23,10 +24,64 @@ function clearValues(){
   bookcase.value = ""
 }
 
-function addBookToLibrary(){
-  console.log(title.value, author.value, pages.value, bookcase.value)
-  let newBook = new Book(title.value, author.value, pages.value, bookcase.value, true)
-  clearValues()
-  myLibrary.push(newBook)
+function updateHtml(library){
+  if(library){
+    books.innerHTML = ""
+    console.log("Book inner html ", books)
+    for(let bookNumber in myLibrary){
+      let book = myLibrary[bookNumber]
+      book.id = bookNumber
+      var bookHtml = document.createElement("div")
+      books.appendChild(bookHtml)
+      bookHtml.classList.add("book")
+      bookHtml.innerHTML = `<img src=${book.bookcase} alt="book-case"/>
+      <div class="book-details">
+        <div class="title">
+          <div>Title</div>
+          <div>${book.title}</div>
+        </div>
+        <div class="author">
+          <div>Author</div>
+          <div>${book.author}</div>
+        </div>
+        <div class="number-of-pages">
+          <div>Number of Pages</div>
+          <div>${book.pages}</div>
+        </div>
+        <div class="buttons">
+          <button id="${book.id}" type="button" class="book-read" onclick="readBook(this.id)">Read</button>
+          <button id="${book.id}-${book.title}}" type="button" class="book-delete" onclick="deleteBook(this.id)">Delete</button>
+        </div>
+      </div>
+      `
+    }
+  }
 }
 
+function addBookToLibrary(){
+  let newBook = new Book(null, title.value, author.value, pages.value, bookcase.value, true)
+  clearValues()
+  myLibrary.push(newBook)
+  updateHtml(myLibrary)
+}
+
+function readBook(id){
+  if(myLibrary[id]){
+    let myButton = document.getElementById(id)
+    if(myLibrary[id].read){
+      myLibrary[id].read = false
+      myButton.textContent = "Unread"
+    } else{
+      myLibrary[id].read = true
+      myButton.textContent = "Read"
+    }
+  }
+}
+
+function deleteBook(id){
+  let myButtonId = id.split("-")
+  myButtonId = id[0]
+  let myBookDiv = document.getElementById(myButtonId).parentElement.parentElement.parentElement
+  books.removeChild(myBookDiv)
+  myLibrary.splice(id,1)
+}
